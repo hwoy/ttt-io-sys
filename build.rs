dbd632615aac35e300c8c68e977603c6fd590447
Watthanachai Dueanklang <bosskillerz@gmail.com>
2023-11-08 18:04:55 +0700

    Initial commit

---
+++ build.rs
@@ -0,0 +1,29 @@
+use std::path::Path;
+
+fn main() {
+    {
+        let bindings = bindgen::Builder::default()
+            .header("u-tic-tac-toe/ttt_io.h")
+			.newtype_enum("ox_gameid")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            .generate()
+            .expect("Unable to generate bindings");
+
+        bindings
+            .write_to_file("src/ttt-io.rs")
+            .expect("Couldn't write bindings!");
+    }
+
+    {
+        let lib = "ttt_io";
+        let path = ["u-tic-tac-toe/ttt_io.c"];
+
+        if path.iter().all(|p| Path::new(p).exists()) {
+            let mut builder = cc::Build::new();
+            for p in path.iter() {
+                builder.file(p);
+            }
+            builder.extra_warnings(true).compile(lib);
+        }
+    }
+}
