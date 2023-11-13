3371a5799f5eec3ec9f8e475fb8db72f3ace782d
Watthanachai Dueanklang <bosskillerz@gmail.com>
2023-11-13 17:27:30 +0700

    add wasi

--- build.rs
+++ build.rs
@@ -2,12 +2,17 @@ use std::path::Path;
 
 fn main() {
     {
-        let emsdk_include_dir_var = "EMSDK_INCLUDE_DIR";
-
         let bindings = bindgen::Builder::default()
             .header("u-tic-tac-toe/ttt_io.h")
             .allowlist_file("u-tic-tac-toe/ttt_io.h")
-            .clang_arg(if let Ok(val) = std::env::var(emsdk_include_dir_var) {
+            .clang_arg(if let Ok(val) = std::env::var("EMSDK_INCLUDE_DIR") {
+                let mut set_arg = "-I".to_string();
+                set_arg.push_str(&val);
+                set_arg
+            } else {
+                "".to_string()
+            })
+            .clang_arg(if let Ok(val) = std::env::var("WASI_INCLUDE_DIR") {
                 let mut set_arg = "-I".to_string();
                 set_arg.push_str(&val);
                 set_arg
